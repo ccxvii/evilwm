5ce8a4635fbe17776bd7eb4d271a1ac0692bc6c8
Ciaran Anscomb <evilwm@6809.org.uk>
2021-07-14 08:00:30 +0100

    list.c formatting changes only

--- list.c
+++ list.c
@@ -15,9 +15,9 @@
 
 // Wrap data in a new list container
 static struct list *list_new(void *data) {
-	struct list *new;
-	new = malloc(sizeof(struct list));
-	if (!new) return NULL;
+	struct list *new = malloc(sizeof(*new));
+	if (!new)
+		return NULL;
 	new->next = NULL;
 	new->data = data;
 	return new;
@@ -26,12 +26,14 @@ static struct list *list_new(void *data) {
 // Insert new data before given position
 struct list *list_insert_before(struct list *list, struct list *before, void *data) {
 	struct list *elem = list_new(data);
-	struct list *iter;
-	if (!elem) return list;
-	if (!list) return elem;
+	if (!elem)
+		return list;
+	if (!list)
+		return elem;
 	elem->next = before;
-	if (before == list) return elem;
-	for (iter = list; iter; iter = iter->next) {
+	if (before == list)
+		return elem;
+	for (struct list *iter = list; iter; iter = iter->next) {
 		if (iter->next == before) {
 			iter->next = elem;
 			return list;
@@ -58,9 +60,11 @@ struct list *list_append(struct list *list, void *data) {
 // Delete list element containing data
 struct list *list_delete(struct list *list, void *data) {
 	struct list **elemp;
-	if (!data) return list;
+	if (!data)
+		return list;
 	for (elemp = &list; *elemp; elemp = &(*elemp)->next) {
-		if ((*elemp)->data == data) break;
+		if ((*elemp)->data == data)
+			break;
 	}
 	if (*elemp) {
 		struct list *elem = *elemp;
@@ -72,22 +76,23 @@ struct list *list_delete(struct list *list, void *data) {
 
 // Move existing list element containing data to head of list
 struct list *list_to_head(struct list *list, void *data) {
-	if (!data) return list;
+	if (!data)
+		return list;
 	list = list_delete(list, data);
 	return list_prepend(list, data);
 }
 
 // Move existing list element containing data to tail of list
 struct list *list_to_tail(struct list *list, void *data) {
-	if (!data) return list;
+	if (!data)
+		return list;
 	list = list_delete(list, data);
 	return list_append(list, data);
 }
 
 // Find list element containing data
 struct list *list_find(struct list *list, void *data) {
-	struct list *elem;
-	for (elem = list; elem; elem = elem->next) {
+	for (struct list *elem = list; elem; elem = elem->next) {
 		if (elem->data == data)
 			return elem;
 	}
